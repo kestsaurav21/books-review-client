ae16e2d1bc9a88779a2512563ff8ddb9adb0d7f3
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-21 17:41:33 +0530

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import { useState } from 'react';
 import './App.css'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import BookList from './pages/BookList';
+import Signup from './pages/SignUp';
+import Login from './pages/Login';
 
 function App() {
   
   return (
     <>
-    <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    <Footer />
+
+    <Router>
+      <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signup' element={<Login />} />
+          <Route path='/booklist' element={<BookList />} />
+          
+        </Routes>
+      <Footer />
+    </Router>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BookContext, BookProvider } from './context/BookContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <BookProvider>
+        <App />
+  </BookProvider>
 )
